daef4f83408d9fd424dfd3fef72b92e835b10f3b
liuzhihui <liuyan2526@qq.com>
2018-08-22 18:35:57 +0800

    f

--- src/Layout/index.js
+++ src/Layout/index.js
@@ -7,9 +7,10 @@ Layout.prototype = {
     _option: {},
     init: function (option) {
         this._option = {
-            parentLayout: null,
             selfLayout: null,
-            direction: null,
+            childrenLayout: null,
+            width: null,
+            height: null,
             childrenAve: false,
             childrenEl: null
         }
@@ -17,19 +18,18 @@ Layout.prototype = {
     },
 
     template: function() {
-        let c = this._option.parentLayout === 'h' ? 'owl-layout-h-' : 'owl-layout-v-'
-        c += this._option.direction === 'left' ? 'left' : ''
-        c += this._option.direction === 'right' ? 'right' : ''
-        c += this._option.direction === 'center' ? 'center' : ''
-        c += this._option.direction === 'top' ? 'top' : ''
-        c += this._option.direction === 'bottom' ? 'bottom' : ''
         let node = document.createElement('div')
-        node.innerHTML = this._option.childrenEl
         node.classList.add('owl-layout-container')
-        node.classList.add(c)
-        if(this._option.selfLayout === 'h') {
+        if(this._option.childrenLayout === 'h') {
             node.classList.add('owl-layout-h')
         }
+        if(this._option.selfLayout === 'h') {
+            node.style.width = this._option.width
+            node.style.float = 'left'
+        } else {
+            node.style.height = this._option.height
+        }
+        node.innerHTML = this._option.childrenEl
         return node
     },
 
@@ -54,30 +54,99 @@ Layout.prototype = {
 }
 
 function autoRender() {
-    let els = document.getElementsByTagName('layout')
-    console.log(els)
-    for (let el of els) {
-        let parentEl = el.parentNode
-        let parentLayout = (parentEl.attributes.hasOwnProperty('childrenlayout') && parentEl.attributes.childrenlayout.value === 'h') ? 'h' : 'v'
-        if(parentLayout === 'v') {
-            for (let c of parentEl.classList) {
-                if(c === 'owl-layout-h') {
-                    parentLayout = 'h'
+    let rootContainer = document.getElementsByTagName('owl-layout')[0].parentNode
+    let targetContainer = rootContainer.cloneNode(true)
+    traversalNodes(rootContainer, targetContainer)
+    rootContainer.replaceChild(targetContainer.getElementsByClassName('owl-layout-container')[0], rootContainer.getElementsByTagName('owl-layout')[0])
+}
+
+function getNodeCnf(node) {
+    let parentNode = node.parentNode
+    let selfLayout = parentNode.attributes.hasOwnProperty('childrenlayout') && parentNode.attributes.childrenlayout.value === 'h' ? 'h' : 'v'
+    let childrenLayout = node.attributes.hasOwnProperty('childrenlayout') ? node.attributes["childrenlayout"].value : 'v'
+    let width, height
+    if (selfLayout === 'h') {
+        if(node.attributes.hasOwnProperty('width')) {
+            width = node.attributes.width.value
+        } else if(node.attributes.hasOwnProperty('flex')) {
+            for (let i = 0; i < parentNode.childNodes.length; i++) {
+
+            }
+        } else {
+            width = 'calc((100%'
+            let j = 0
+            for (let i = 0; i < parentNode.childNodes.length; i++) {
+                if(parentNode.childNodes[i].nodeName === "OWL-LAYOUT") {
+                    j++
+                }
+                if(parentNode.childNodes[i].nodeName === "OWL-LAYOUT" && parentNode.childNodes[i].attributes.hasOwnProperty('width')) {
+                    width += ' - ' + parentNode.childNodes[i].attributes.width.value
+                    j--
                 }
             }
+            width += ')'
+            if(j !== 0) {
+                width += ' / ' + j
+            }
+            width += ')'
         }
-        let selfLayout = el.attributes.hasOwnProperty('layout') ? el.attributes.layout.value : 'v'
-        let direction = el.attributes.hasOwnProperty('dir') ? el.attributes.dir.value : ''
-        let layout = new Layout({
-            parentLayout: parentLayout,
-            selfLayout: selfLayout,
-            direction: direction,
-            childrenEl: el.innerHTML
-        })
-        let temp = layout.template()
-        console.log(temp)
-        el.parentNode.replaceChild(temp, el)
-        autoRender()
+    } else {
+        if(node.attributes.hasOwnProperty('height')) {
+            height = node.attributes.height.value
+        } else if(node.attributes.hasOwnProperty('flex')) {
+            for (let i = 0; i < parentNode.childNodes.length; i++) {
+
+            }
+        } else {
+            height = 'calc((100%'
+            let j = 0
+            for (let i = 0; i < parentNode.childNodes.length; i++) {
+                if(parentNode.childNodes[i].nodeName === "OWL-LAYOUT") {
+                    j++
+                }
+                if(parentNode.childNodes[i].nodeName === "OWL-LAYOUT" && parentNode.childNodes[i].attributes.hasOwnProperty('height')) {
+                    height += ' - ' + parentNode.childNodes[i].attributes.height.value
+                    j--
+                }
+            }
+            height += ')'
+            if(j !== 0) {
+                height += ' / ' + j
+            }
+            height += ')'
+        }
+
+    }
+    return {
+        selfLayout: selfLayout,
+        childrenLayout: childrenLayout,
+        width: width === 'calc(100%)' ? '100%' : width,
+        height: height === 'calc(100%)' ? '100%' : height,
+        childrenEl: node.innerHTML
+    }
+}
+function createNode(cnf) {
+    let layout = new Layout(cnf)
+    let temp = layout.template()
+    return temp
+}
+function traversalNodes(node, targetNode) {
+    if(node && node.nodeName === "OWL-LAYOUT"){
+        let cnf = getNodeCnf(node)
+        let createdNode = createNode(cnf)
+        targetNode.parentNode.replaceChild(createdNode, targetNode)
+        targetNode = createdNode
+    }
+    let childLength = node.childNodes.length
+    let oldChildNodes = node.childNodes, oldChildNode;
+    let targetChildNodes = targetNode.childNodes, targetChildNode;
+    for(let i = 0; i < childLength ; i++){
+        oldChildNode = oldChildNodes[i]
+        targetChildNode = targetChildNodes[i]
+        if(oldChildNode.nodeName === "OWL-LAYOUT"){
+            //递归先序遍历子节点
+            traversalNodes(oldChildNode, targetChildNode);
+        }
     }
 }
 
