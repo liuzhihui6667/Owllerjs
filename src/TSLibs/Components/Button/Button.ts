a03a54a72d600f0308311f1b213c93936a89da6d
liuzhihui <liuyan2526@qq.com>
2018-10-17 18:23:59 +0800

    btn

---
+++ src/TSLibs/Components/Button/Button.ts
@@ -0,0 +1,128 @@
+import {Components} from '../../Interfaces/Component'
+import {OWLNODE} from '../../OwlNode/OWLNODE'
+import '../../../Style/Button/index.less'
+import {IconComponent} from "../Icon/Icon";
+
+class ButtonComponent extends Components {
+    type: string;
+    size: string;
+    icon: string;
+    circle: boolean;
+    innerText: string;
+    innerElement: Element;
+    attr: object;
+    constructor(innerText?: string, type?: string, size?: string, icon?: string, circle?: boolean, attr?:object) {
+        super();
+        this.innerText = innerText === undefined ? 'Button' : innerText;
+        this.type = type === undefined ? 'default' : type;
+        this.size = size === undefined ? 'default' : size;
+        this.icon = icon === undefined ? '' : icon;
+        this.circle = circle === undefined ? false : circle;
+        this.attr = attr === undefined ? {} : attr
+        this.init();
+    }
+
+    init() {
+        this._getTemplate();
+        this._setEvent();
+    }
+
+    _getTemplate() {
+        if(this.node !== undefined) {
+            return this.node;
+        }
+        let node = document.createElement('div');
+        node.classList.add('owl-btn-container');
+        for (let index in this.attr) {
+            if(this.attr[index].name === 'class') {
+                let class_list = this.attr[index].value.split(' ')
+                for (let i = 0; i < class_list.length; i++) {
+                    node.classList.add(class_list[i])
+                }
+                continue
+            }
+            node.setAttribute(this.attr[index].name, this.attr[index].value)
+        }
+        if(this.circle) {
+            node.classList.add('owl-btn-circle');
+        }
+        let btnHeight = ''
+        switch (this.size) {
+            case 'small':
+                node.classList.add('owl-btn-small')
+                btnHeight = '28px';
+                break
+            case 'large':
+                node.classList.add('owl-btn-large')
+                btnHeight = '40px'
+                break
+            default:
+                node.classList.add('owl-btn-default')
+                btnHeight = '34px'
+                break
+        }
+        switch (this.type) {
+            case 'error':
+                node.classList.add('owl-btn-t-error')
+                break
+            case 'success':
+                node.classList.add('owl-btn-t-success')
+                break
+            case 'disabled':
+                node.classList.add('owl-btn-t-disabled')
+                break
+            case 'lighter':
+                node.classList.add('owl-btn-t-lighter')
+                break
+            case 'dark':
+                node.classList.add('owl-btn-t-dark')
+                break
+            default:
+                node.classList.add('owl-btn-t-default')
+                break
+        }
+        if(this.icon !== '') {
+            let iconSize = '';
+            switch (this.size) {
+                case 'small':
+                    iconSize = '14px';
+                    break;
+                case 'large':
+                    iconSize = '20px';
+                    break;
+                default:
+                    iconSize = '18px';
+                    break
+            }
+            let iconColor = this.type === 'disabled' ? '#c5c8ce' : '#ffffff'
+            switch (this.type) {
+                case 'disabled':
+                    iconColor = '#c5c8ce'
+                    break
+                case 'lighter':
+                    iconColor = '#515a6e'
+                    break
+                default:
+                    iconColor = '#ffffff'
+            }
+
+            let icon = new IconComponent(this.icon, iconSize, btnHeight, iconColor)
+            var iconNode = icon._getTemplate();
+            iconNode.classList.add('owl-btn-icon')
+            node.appendChild(iconNode)
+        }
+        let textSpan = document.createElement('span');
+        textSpan.classList.add('owl-btn-text-wrapper');
+        textSpan.innerText = this.innerText;
+        node.appendChild(textSpan);
+        this.node = node
+        return node
+    }
+
+    _setEvent() {
+        this.node
+    }
+
+}
+
+export {ButtonComponent}
--- src/entry.ts
+++ src/entry.ts
@@ -1,4 +1,5 @@
 import {NavigationComponent} from './TSLibs/Components/Navigation/Navigation'
+import {ButtonComponent} from "./TSLibs/Components/Button/Button";
 
 let u = new NavigationComponent('v', [
     {
@@ -42,10 +43,58 @@ let u = new NavigationComponent('v', [
             }
         ]
     }
-], true, true, false, 'dark')
+], false, true, false, 'dark')
 
-document.getElementsByClassName('container')[0].appendChild(u._getTemplate())
+// document.getElementsByClassName('container')[0].appendChild(u._getTemplate())
 
+
+let bs = new ButtonComponent('按钮', 'disabled', 'small', 'store')
+// b._getTemplate()
+document.getElementsByClassName('container')[0].appendChild(bs.node)
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+let bd = new ButtonComponent('按钮', 'disabled', 'default', 'store')
+// b._getTemplate()
+document.getElementsByClassName('container')[0].appendChild(bd.node)
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+
+let bl = new ButtonComponent('按钮', 'disabled', 'large', 'store')
+// b._getTemplate()
+document.getElementsByClassName('container')[0].appendChild(bl.node)
+
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
+
+let cs = new ButtonComponent('按钮', 'dark', 'small', 'coins')
+
+document.getElementsByClassName('container')[0].appendChild(cs._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+
+let cd = new ButtonComponent('按钮', 'dark', '', 'coins')
+
+document.getElementsByClassName('container')[0].appendChild(cd._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+
+let cl = new ButtonComponent('按钮', 'success', 'large', '')
+
+document.getElementsByClassName('container')[0].appendChild(cl._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
+
+let ds = new ButtonComponent('按钮', 'error', 'small', 'coins', true)
+
+document.getElementsByClassName('container')[0].appendChild(ds._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+
+let dd = new ButtonComponent('按钮', '', '', 'coins', true)
+
+document.getElementsByClassName('container')[0].appendChild(dd._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('img'))
+
+let dl = new ButtonComponent('按钮', 'lighter', 'large', 'coins', true)
+
+document.getElementsByClassName('container')[0].appendChild(dl._getTemplate())
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
+document.getElementsByClassName('container')[0].appendChild(document.createElement('br'))
 
 
 // import Components from './Components/components.js'
