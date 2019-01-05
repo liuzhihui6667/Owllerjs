356d934fe160c9ef996b8c565db99beaf9733bb6
liuzhihui <liuyan2526@qq.com>
2019-01-05 23:24:58 +0800

    j

--- src/TSLibs/Components/Roller/Roller.ts
+++ src/TSLibs/Components/Roller/Roller.ts
@@ -3,33 +3,97 @@ import '../../../Style/Roller/index.less'
 import {IconComponent} from "../Icon/Icon";
 
 class RollerComponent extends Components {
+    /**
+     * 初始渲染展示子元素的序号，从0开始
+     */
     curValue: number;
+    /**
+     * 是否循环展示
+     */
     loop: boolean;
+    /**
+     * 是否自动播放
+     */
     auto: boolean;
+    /**
+     * 自动播放间隔毫秒数, auto为false时该值无效
+     */
     speed: number;
+    /**
+     * 指示器类型
+     * dot: 点状指示器
+     * line: 线状指示器
+     * none: 不展示指示器
+     */
     tip: string;
-    height: string;
-    width: string;
+    /**
+     * 滚动方向,默认左右滚动
+     */
+    dir: string;
+    /**
+     * 该组件的样式,默认为2d
+     * 3d: 3d模式
+     */
+    type: string;
+    /**
+     * 一屏展示的子元素数目
+     */
+    showNum: number;
+    /**
+     * 一次滚动子元素的数目
+     */
+    scrollNum: number;
+    /**
+     * 整体高度,默认为250px
+     */
+    height: number;
+    /**
+     * 整体宽度,默认为500px
+     */
+    width: number;
+    /**
+     * 子元素数组列表
+     */
     itemList: Array<HTMLElement>;
+    /**
+     * 定时器
+     */
     intervel: number;
-    constructor(type?: string,
+
+    itemWidth: number;
+
+    itemHeight: number;
+
+    constructor(dir?: string,
                 curVulue?: number,
                 itemList?: Array<HTMLElement>,
                 loop?: boolean,
                 auto?: boolean,
                 speed?: number,
                 tip?: string,
-                height?: string,
-                width?: string) {
+                height?: number,
+                width?: number,
+                showNum?: number,
+                scrollNum?: number) {
         super()
+        this.dir = dir === undefined ? 'h' : dir
         this.curValue = curVulue === undefined ? 0 : curVulue
         this.loop = loop === undefined ? false : loop
         this.auto = auto === undefined ? false : auto
         this.speed = speed === undefined ? 3000 : speed
         this.tip = tip === undefined ? 'none' : tip
         this.itemList = itemList === undefined ? [] : itemList
-        this.height = height === undefined ? '250px' : height
-        this.width = width === undefined ? '500px' : width
+        this.height = height === undefined ? 250 : height
+        this.width = width === undefined ? 500 : width
+        this.showNum = showNum === undefined ? 1 : showNum
+        this.scrollNum = scrollNum === undefined ? 1 : scrollNum
+        if(this.dir === 'h') {
+            this.itemWidth = 1/this.showNum*this.width
+            this.itemHeight = this.height
+        } else {
+            this.itemWidth = this.width
+            this.itemHeight = 1/this.showNum*this.height
+        }
         this.init()
     }
     init(): void {
@@ -47,8 +111,8 @@ class RollerComponent extends Components {
     __getNode(): Element {
         let node = document.createElement('div')
         node.classList.add('owl-roller-container')
-        node.style.height = this.height
-        node.style.width = this.width
+        node.style.height = this.height + 'px'
+        node.style.width = this.width + 'px'
         let itemNodeWrapper = document.createElement('div')
         itemNodeWrapper.classList.add('owl-roller-item-container')
         let itemSliderWrapper = document.createElement('div')
@@ -67,12 +131,23 @@ class RollerComponent extends Components {
                 tipWrapper.classList.add('owl-roller-tip-dot')
                 break
         }
+        let itemWidth: number, itemHeight: number
         for (let i = 0; i < this.itemList.length; i++) {
             let itemNode = document.createElement('div')
             itemNode.classList.add('owl-roller-item-wrapper')
-            if(i === this.curValue) {
+            itemNode.style.width = this.itemWidth + 'px'
+            itemNode.style.height = this.itemHeight + 'px'
+            if(i >= this.curValue && i < this.curValue + this.showNum) {
+                if(i === this.curValue) {
+                    itemNode.classList.add('owl-roller-cur')
+                }
                 itemNode.style.display = 'block'
-                itemNode.classList.add('owl-roller-cur')
+                if(this.dir === 'h') {
+                    itemNode.style.left = ((i - this.curValue)*this.itemWidth).toFixed(2) + 'px'
+                } else {
+                    itemNode.style.top = ((i - this.curValue)*this.itemHeight).toFixed(2) + 'px'
+                }
+
             }
             itemNode.style.transform = 'translateX(0)'
             let itemel = this.itemList[i]
@@ -164,23 +239,23 @@ class RollerComponent extends Components {
         clearInterval(this.intervel)
     }
 
-    __move(dir: string, moveNum: number = 1): void {
+    __move(dir: string, moveNum: number = 3): void {
         if(!this.loop) {
             if(dir === 'left' && this.curValue === this.itemList.length - 1 || dir === 'right' && this.curValue === 0) {
                 return
             }
         }
         let wrapper = this.node.getElementsByClassName('owl-roller-item-slider-wrapper')[0]
-        wrapper.style.width = 100 * (moveNum + 1) + '%'
+        wrapper.style.width = (this.itemWidth * (this.showNum + moveNum)).toFixed(2) + 'px'
         wrapper.style.left = 'none'
         wrapper.style.right = 'none'
         wrapper.style.transform = 'translate(0%)'
         wrapper.style.transition = 'none'
         let itemElList = wrapper.children
         for (let i = 0; i < this.itemList.length; i++) {
-            itemElList[i].style.width = 1/(1+moveNum)*100 + '%'
+            // itemElList[i].style.width = 1/(1+moveNum)*100 + '%'
             itemElList[i].style.transform = 'translateX(0%)'
-            if(i !== this.curValue) {
+            if(i < this.curValue && i >= this.curValue + this.showNum) {
                 itemElList[i].style.display = 'none'
             }
         }
@@ -191,14 +266,16 @@ class RollerComponent extends Components {
                 this.__tipShow(this.curValue, newValue)
                 wrapper.style.left = '0px'
                 for (let i = 1; i <= moveNum; i++) {
-                    let j = (this.curValue + i) % this.itemList.length
+                    let j = (this.curValue + this.showNum + i - 1) % this.itemList.length
                     itemElList[j].style.display = 'block'
-                    itemElList[j].style.transform = 'translateX('+100*i+'%)'
+                    itemElList[j].style.transform = 'translateX('+((this.showNum + i - 1) * this.itemWidth).toFixed(2)+'px)'
                 }
+                // return
                 this.curValue = newValue
+                let that = this
                 setTimeout(function () {
                     wrapper.style.transition = 'transform .3s'
-                    wrapper.style.transform = 'translateX(-' + 100 * moveNum/(moveNum + 1) + '%)'
+                    wrapper.style.transform = 'translateX(-' + ((that.itemWidth * moveNum).toFixed(2)) + 'px)'
                 }, 10)
                 break
             case 'right':
--- src/TSLibs/Renders/Roller/Roller.ts
+++ src/TSLibs/Renders/Roller/Roller.ts
@@ -2,26 +2,28 @@ import {RollerComponent} from "../../Components/Roller/Roller";
 
 
 class RollerRender {
-
+    dir: string;
     curValue: number;
     loop: boolean;
     auto: boolean;
     speed: number;
     tip: string;
-    height: string;
-    width: string;
+    height: number;
+    width: number;
     itemList: Array<HTMLElement> = [];
+    showNum: number;
     constructor() {
         this.render()
     }
 
     render(): void {
         let tags = this.getNodes()
-        for (let i = 0; i < tags.length; i++) {
-            this.getConfig(tags[i])
-            let retEl = new RollerComponent('9', this.curValue, this.itemList, this.loop,
-                this.auto, this.speed, this.tip, this.height, this.width)
-            tags[i].replaceWith(retEl.node)
+        let tagsCount = tags.length
+        for (let i = 0; i < tagsCount; i++) {
+            let itemList = this.getConfig(tags[0])
+            let retEl = new RollerComponent(this.dir, this.curValue, itemList, this.loop,
+                this.auto, this.speed, this.tip, this.height, this.width, this.showNum)
+            tags[0].replaceWith(retEl.node)
         }
     }
 
@@ -30,20 +32,24 @@ class RollerRender {
         return nodes
     }
 
-    getConfig(tag: Element): void {
+    getConfig(tag: Element): Array<HTMLElement> {
+        this.dir = tag.getAttribute('dir') === null ? 'h' : tag.getAttribute('dir')
         this.curValue = tag.getAttribute('value') === null ? 0 : parseInt(tag.getAttribute('value'))
         this.loop = tag.getAttribute('loop') === null ? false : (tag.getAttribute('loop') === 'true')
         this.auto = tag.getAttribute('auto') === null ? false : (tag.getAttribute('auto') === 'true')
         this.speed = tag.getAttribute('speed') === null ? 3000 : parseInt(tag.getAttribute('speed'))
         this.tip = tag.getAttribute('tip') === null ? 'dot' : tag.getAttribute('tip')
-        this.height = tag.getAttribute('height') === null ? '250px' : tag.getAttribute('height')
-        this.width = tag.getAttribute('width') === null ? '500px' : tag.getAttribute('width')
+        this.height = tag.getAttribute('height') === null ? 250 : parseInt(tag.getAttribute('height'))
+        this.width = tag.getAttribute('width') === null ? 500 : parseInt(tag.getAttribute('width'))
+        this.showNum = tag.getAttribute('shownum') === null ? 1 : parseInt(tag.getAttribute('shownum'))
         let tagLength = tag.children.length
+        let itemList: Array<HTMLElement> = []
         for (let i = 0; i < tagLength; i++) {
             let div = document.createElement('div')
             div.appendChild(tag.children[0])
-            this.itemList.push(div)
+            itemList.push(div)
         }
+        return itemList
     }
 }
 
